1cbd46771c0ee33f3c560bb44ff2e08a2f8487f3
deyan petrov <deyanp19@hotmail.com>
2022-07-26 20:48:19 +0300

    notify added

--- src/app.js
+++ src/app.js
@@ -7,13 +7,16 @@ import decorateContext from './middlewares/render.js';
 import addSession from './middlewares/session.js';
 import { homePage } from './views/home.js';
 import { loginPage } from './views/login.js';
-import { notify } from './lib/notify.js';
+ 
+import notify from './middlewares/notify.js'
+
 
 page(addSession() )
 page(decorateContext());
+page(notify())
 page('/',homePage);
 page('/login',loginPage)
 
 page.start();
  
-window.notify = notify
+ 
--- src/lib/notify.js
+++ src/lib/notify.js
@@ -1,14 +1,23 @@
 const container = document.createElement('div');
 container.id='notification';
 const list = document.createElement('ul');
-
+list.addEventListener('click',onClick)
 container.appendChild(list);
 
 document.body.appendChild(container);
 
 export function notify(message) {
     const liItem = document.createElement('li');
-    liItem.className='notification'
-    liItem.textContent=message;
-    list.appendChild(liItem)
+    liItem.className='notification';
+
+    liItem.textContent=message+' \u2716';
+    list.appendChild(liItem);
+
+    setTimeout(()=>liItem.remove(),2000)
+}
+
+function onClick(event) {
+    if (event.target.tagName =='LI'){
+        event.target.remove();
+    }
 }
